e670668eee63fe94f62e714aefd777b05d5e9122
artcalciolari <arthurcalciolari@gmail.com>
2022-10-25 15:33:02 -0300

    on game.py: added the remaining code for the initial interface/menu

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,4 +1,5 @@
 from operator import truediv
+from turtle import screensize
 import pygame
 
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, DEFAULT_TYPE
@@ -90,4 +91,66 @@ class Game:
         self.power_up_manager.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
+
+    def draw_background(self):
+        image_width = BG.get_width()
+        self.screen.blit(BG, (self.x_pos_bg, self.y_pos_bg))
+        self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
+        if self.x_pos_bg <= - image_width:
+            self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
+            self.x_pos_bg = 0
+            self.x_pos_bg -= self.game_speed
+
+    def draw_score(self):
+       draw_message_component(
+        f"Score: {self.score}",
+        self.screen,
+        pos_x_center=1000,
+        pos_y_center=50
+        )
     
+    def draw_power_up_time(self):
+        if self.player.has_power_up:
+            time_to_show = round((self.player.power_up_time - pygame.time.get_ticks()) / 1000, 2)
+            if time_to_show >= 0:
+                draw_message_component(
+                    f'{self.player.type.capitalize()} enabled for {time_to_show} seconds',
+                    self.screen,
+                    font_size = 18,
+                    pos_x_center=500,
+                    pos_y_center=40
+                    )
+            else:
+                self.player.has_power_up = False
+                self.player.type = DEFAULT_TYPE
+    
+    def handle_events_on_menu(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+                self.running = False
+            elif event.type == pygame.KEYDOWN:
+                self.run()
+
+    def show_menu(self):
+        self.screen.fill((255, 255, 255))
+        half_screen_height = SCREEN_WIDTH // 2
+        half_screen_width = SCREEN_WIDTH // 2
+        if self.death_count == 0:
+            draw_message_component("Press any key to start", self.screen)
+        else:
+            draw_message_component("Press any key if you wish to restart", self.screen, pos_y_center= half_screen_height + 140)
+            draw_message_component(
+                f'Your score: {self.score}',
+                self.screen,
+                pos_y_center= half_screen_height - 150
+            )
+            draw_message_component(
+                f'Death Count: {self.death_count}',
+                self.screen,
+                pos_y_center= half_screen_height - 100
+            )
+            self.screen.blit(ICON, (self.screen_width - 40, half_screen_height -30))
+        
+        pygame.display.flip()
+        self.handle_events_on_menu()
